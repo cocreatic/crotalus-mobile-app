e25f172f26c80546b402125ec85e9788baaea280
Leonardo Pérez M <leono286@gmail.com>
2020-04-29 00:55:38 -0500

    update URL to retrieve the list of available repositories to connect to

--- src/app/settings/repositories/repositories.component.ts
+++ src/app/settings/repositories/repositories.component.ts
@@ -19,7 +19,7 @@ export class RepositoriesComponent implements OnInit, OnDestroy {
   nonConnectedRepositories: BoaRepository[];
   currentToast: null | HTMLIonToastElement = null;
 
-  registeredRepositoriesUrl = 'http://b.boaproject.net/index.json';
+  registeredRepositoriesUrl = 'http://b.cocreatic.org/index.json';
   registeredRepositories: any;
   initializing = true;
 
